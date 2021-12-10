9e73ced016d965cd8df70ab15ba8858d92d98e41
Ivaylo Ivanov <ivaylozivanov1@gmail.com>
2021-12-10 18:59:31 +0200

    base style

---
+++ src/components/Navigation.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "../style/navigation.css";
+
+const Navigation = () => {
+  return (
+    <nav>
+      <div className="in-nav">
+        <div className="section-1">
+          <img src="#" alt="logo" />
+          <h1>Macro Brain</h1>
+        </div>
+        <div className="section-2">
+          <p>Sign Out</p>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navigation;
