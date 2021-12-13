80e0b5d62d1a1cb0530bcfc2623edc01df85ccd9
Ivaylo Ivanov <ivaylozivanov1@gmail.com>
2021-12-13 11:41:23 +0200

    making it responsive

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./style/index.css";
+import "./style/responsive.css";
 import "./style/global.css";
 import App from "./containers/App";
 import reportWebVitals from "./reportWebVitals";
